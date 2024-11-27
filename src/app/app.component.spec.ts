61e37140cf9c6bd14fed50f7fc6cc5d82e989073
Welliton Altimari <wellitonaltimari@tokenlab.com.br>
2024-11-27 19:32:35 -0300

    lint and bootstrap

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,19 +11,23 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-pokedex' title`, () => {
+  it('should have the \'angular-pokedex\' title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+
     expect(app.title).toEqual('angular-pokedex');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
+
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-pokedex');
   });
 });
